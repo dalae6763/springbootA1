8e0c696a69db2a5d19128a29f2f0dabbd628d026
yonggyo1125 <yonggyo00@kakao.com>
2023-04-20 09:52:24 +0900

    강의 소스 업로드

--- exam02/src/main/java/org/koreait/repositories/MemberRepository.java
+++ exam02/src/main/java/org/koreait/repositories/MemberRepository.java
@@ -1,8 +1,22 @@
 package org.koreait.repositories;
 
 import org.koreait.entities.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Member findByUserId(String userId);
+
+    List<Member> findByUserIdNot(String userId); // userId <> ...
+    List<Member> findByUserNmContaining(String key); // userNm LIKE '%:key%'
+
+    List<Member> findByRegDtBetween(LocalDate sDate, LocalDate eDate, Pageable pageable);
+
+    List<Member> findByRegDtBetweenOrderByRegDtDesc(LocalDate sDate, LocalDate eDate);
+
+
+
 }
